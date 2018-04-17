49d9da6ea250232c022090d5c9396495365d9d83
Peter Suchý <suchype1@fel.cvut.cz>
2018-04-18 00:53:29 +0200

    Finished login page

--- src/main/java/gui/LoginView.java
+++ src/main/java/gui/LoginView.java
@@ -1,52 +1,73 @@
 package gui;
+
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LoginView{
 
     private static JFrame jf;
-    private static JPanel jp;
+    private final JTextField text;
+    private final JTextField errtext;
+    private JLabel userLabel, passwordLabel;
+    private JPasswordField passwordText;
+    private JTextField userText;
+    private JButton loginButton, resetButton;
+
 
     public LoginView(){
+        this.text = new JTextField("Succesfully logged");
+        this.errtext = new JTextField("Incorrect login or password");
         jf = new JFrame("LOGIN MENU");
-        jf.setSize(300, 200);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        jp = new JPanel();
-        jf.add(jp);
-        placeComponents(jp);
+        this.userLabel = new JLabel("User:");
+        this.passwordLabel = new JLabel("Password:");
+        this.loginButton = new JButton("Login");
+        this.resetButton = new JButton("Clear");
+        this.userText = new JTextField(20);
+        this.passwordText = new JPasswordField(20);
+
+        userLabel.setBounds(30, 20, 100, 40);
+        passwordLabel.setBounds(30, 60, 80, 25);
+        userText.setBounds(100, 30, 160, 25);
+        passwordText.setBounds(100, 60, 160, 25);
+        loginButton.setBounds(50, 100, 90, 35);
+        resetButton.setBounds(160, 100, 90, 35);
+
+        jf.setSize(300, 180);
         jf.setLocationRelativeTo(null);
         jf.setVisible(true);
-    }
 
-    private static void placeComponents(JPanel panel) {
+        jf.setLayout(null);
 
-        panel.setLayout(null);
 
-        JLabel userLabel = new JLabel("Login:");
-        userLabel.setBounds(30, 20, 100, 40);
-        panel.add(userLabel);
+        jf.add(userLabel);
+        jf.add(userText);
+        jf.add(passwordLabel);
+        jf.add(passwordText);
+        jf.add(loginButton);
+        jf.add(resetButton);
 
-        JTextField userText = new JTextField(20);
-        userText.setBounds(100, 30, 160, 25);
-        panel.add(userText);
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String uname = userText.getText();
+                String pass = new String(passwordText.getPassword());
+                if (uname.equals("Admin") && (pass.equals("admin"))) {
+                    JOptionPane.showMessageDialog(jf, text.getText());
+                } else {
+                    JOptionPane.showMessageDialog(jf, errtext.getText(), "ERROR", JOptionPane.ERROR_MESSAGE);
+                }
 
-        JLabel passwordLabel = new JLabel("Password:");
-        passwordLabel.setBounds(30, 60, 80, 25);
-        panel.add(passwordLabel);
 
-        JPasswordField passwordText = new JPasswordField(20);
-        passwordText.setBounds(100, 60, 160, 25);
-        panel.add(passwordText);
-
-        JButton loginButton = new JButton("login");
-        loginButton.setBounds(40, 110, 90, 25);
-        panel.add(loginButton);
-
-        JButton registerButton = new JButton("register");
-        registerButton.setBounds(160, 110, 90, 25);
-        panel.add(registerButton);
-
+            }
+        });
+        resetButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                userText.setText("");
+                passwordText.setText("");
+            }
+        });
     }
 
 
