8965a88a24c05752bd140c513bf522f91e7db432
Peter Suchý <suchype1@fel.cvut.cz>
2018-05-21 13:27:51 +0200

    create logs directory

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,6 +2,8 @@ package app;
 
 import gui.main.LoginMenu;
 
+import java.io.File;
+
 /*
 Main class which we use for running program.
 */
@@ -9,6 +11,7 @@ public class Main {
 
 
     public static void main(String[] args){
+        boolean f = new File("logs").mkdir();//create logs directory
         LoginMenu l = new LoginMenu();
         l.actionLogin();
 
